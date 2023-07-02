6cd709e79e582d3bdd188743b8220110abe9c422
andreenakashima <andreenakashima@gmail.com>
2023-07-02 10:13:09 -0300

    Estrutura base da UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Provider as ReduxProvider } from "react-redux";
-import AddTodo from "./components/AddTodo";
-import { TodoList } from "./components/TodoList";
+
 import { store } from "./store";
+import Player from "./components/Player";
+
+import "./styles/global.css";
 
 function App() {
 	return (
 		<ReduxProvider store={store}>
-			<TodoList />
-			<AddTodo />
+			<Player />
 		</ReduxProvider>
 	);
 }
